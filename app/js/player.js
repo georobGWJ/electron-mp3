cf5898967c8eea9591bef576cc1c0634bdf155fb
Robert Turner Jr <georob23@gmail.com>
2017-01-15 12:56:41 -0800

    Add JSON loading function to JS.

--- app/js/player.js
+++ app/js/player.js
@@ -3,11 +3,30 @@ $(document).ready(function() {
     playListener();
     pauseListener();
     songSelectListener();
-
     volumeListener();
+    let song_db;
+    loadJSON(function(response) {
+      song_db = JSON.parse(response);
+     });
+
+    // let song_db = JSON.parse($.getJSON("data/local_media.json"));
+    console.log(song_db);
 });
 
 let audio
+
+var loadJSON = function(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', "data/local_media.json", false);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
 
 var setSong = function(song) {
   audio = new Audio(song);
